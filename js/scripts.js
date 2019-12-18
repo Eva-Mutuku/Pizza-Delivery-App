645074566ed6a772d81fac12b018c77745830c7d
Eva Mutuku <mutileeva@gmail.com>
2019-12-18 10:11:26 -0800

    Calculate total price and show in cart and alert

--- js/scripts.js
+++ js/scripts.js
@@ -35,115 +35,45 @@
           $("#deliveryLocation").show();
         });
 
-        // 
+        // Pizza Constructor
+        function PizzaOrder(pizzaSize, pizzaCrust, pizzaToppings, pizzaCount) {
+          this.size = pizzaSize;
+          this.crust = pizzaCrust;
+          this.toppings = pizzaToppings; //this is an array
+          this.count = pizzaCount;
+      }
 
-            $(".orderSummary").show();
-            // Added variable count for the pizzaCount
-            var pizzaType, pizzaCrust, pizzaToppings, pizzaCount;
-            pizzaType = $(".pizzaType").val();
-            pizzaToppings = $(".form-check-input").val();
-            pizzaCrust = $("#pizzaCrust :selected").val();
-            // Can't find an instance where I'll be needing to reuse the count object so I just called .val() on it
-            pizzaCount = parseInt($("#pizzaCount input").val());
-            
-            // Create Pizza Constructor
-            // Actually it's a pizza
-            function PizzaOrder(type, crust, toppings, count) {
-                this.type = type;
-                this.crust = crust;
-                this.toppings = toppings;
-                this.count = count;
-            }
-            // new pizza order
-            var newOrder = new PizzaOrder(
-                pizzaType,
-                pizzaCrust,
-                pizzaToppings,
-                pizzaCount
-            );
-            console.log(newOrder);
-            // orderSummary(newOrder);
-     
-            //     // var largeSelected, mediumSelected, kidsizeSelected, xlargeSelected;
-            //     var pizzaTypeSelected = $("#pizzaType :selected").attr('class');
-            //     pizzaCrust = $("#pizzaCrust :selected").val();
-            //     let crustprice, TotalPrice;
-            //     if(pizzaCrust === "thin"){
-            //         crustprice = pizzaType(":selected").val();
-            //         console.log(crustprice)
-            //     }
-            //     else if (pizzaCrust === "thick"){
-            //         crustprice = pizzaType(":selected").val() + 100;
-            //         console.log(crustprice)
-            //     }
-            //     else if (pizzaCrust === "deep"){
-            //         crustprice = pizzaType(":selected").val() + 150;
-            //         console.log(crustprice)
-            //     }
-            //     else{
-            //         crustprice = pizzaType(":selected").val() + 200;
-            //         console.log(crustprice)
-            //     }
+      //create a get price prototype
+      pizzaOrder.prototype.getPrice = function () {
+        let sizePrice, crustPrice, toppingsPrice;
+        sizePrice = parseInt(this.size.val());
+        crustPrice = parseInt(this.crust.val());
+        toppingsPrice = this.toppings.map(function() {
+          return parseInt($(this).val());
+        })
 
-            //     console.log(pizzaToppings)
-            //     console.log(pizzaCount)
+        let totalPriceForToppings = 0;
+        for(let i=0; i<toppingsPrice.length; i++){
+          totalPriceForToppings += toppingsPrice[i];
+        }
 
-            //     orderPrice = (crustprice + pizzaToppings) * pizzaCount;
+        letPriceOfOrder = (sizePrice + crustPrice + totalPriceForToppings) * this.count;
+        return orderPrice;
+      };
 
-            //     console.log(orderPrice);
-            // function orderSummary(order) {
-            //         order.crust.val(),
-            //         order.toppings.next("label").text(),
-            //         order.count,
-            //         order.orderPrice
+      // Add pizza to cart
+      function addToCart(order) {
+        let toppings = order.toppings.map(function(){
+          return this.id;
+        })
+        .get()
+        .join();
+        $("#pizzaCart tablebody").append("<tr><td>${order.size.html()}</td><td>${order.crust.html()}</td><td>${toppings}</td><td>${order.getPrice()}</td>}");
 
-            // }
-            // //User Interface
-            // $(document).ready(function() {
-            //   $("#orderForm").submit(function(event) {
-            //     event.preventDefault();
-            //   });
-            //Reveal order summary upon clicking make order.
-            $("#pizzaCraft").submit(function() {
-              $(".orderSummary").show();
-            });
-            $(".Delivery").click(function () {
-                $(".customerContactDetails").show();
-            });
-            //Create summary
-            $("#list").text(" ");
-            $("#list")
-                .append(
-                    "<br>" +
-                    "Type :   " +
-                    newOrder.type.val() +
-                    "<br>" +
-                    "Size :   " +
-                    newOrder.size.val() +
-                    "<br>" +
-                    "Crust :     " +
-                    newOrder.crust.val() +
-                    "<br>" +
-                    " count of pizzas :    " +
-                    newOrder.count +
-                    "<br>" +
-                    " Toppings :    " +
-                    newOrder.toppings.next("label").text() +
-                    "<br>" +
-                    "Total Price :  " +
-                    newOrder.orderPrice() +
-                    "<br><br>"
-                )
-                .css("font-family", "system-ui")
-                .css("font-size", "24px");
-            //   //Write to the order
-            //   $(".summary").slideDown(+toppingsPrice);
-            //   $(".cdata-overlay").slideUp();
-            //   $("#list").slideDown();
-            //   $(".deliver").show(100 + toppingsPrice);
-            //   $(".delivernot").show(100 + toppingsPrice);
-            // });
-        });
-    });
-
-</script>
+        var currentTotalCharge = parseInt($("#total-charge").html());
+        $("#total-charge").html(currentTotalCharge + order.getPrice());
+        $("#checkoutBtn").show();
+        $("#delivery").show();
+      }
+    })
+  
